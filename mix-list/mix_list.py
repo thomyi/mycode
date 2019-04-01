e62278d28d7cb11d2ad7eb812808d178c361e62e
thomyi <thomasyi@gmail.com>
2019-04-01 14:39:36 -0400

    My first commit

---
+++ mix-list/mix_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+print("The first item in the list (IP_: " + my_list[0] )
+
+print("The secoond item in the list (port): " + str(my_list[1]) )
+
+print("The last item in the list (state): " + my_list[2] )
+
+
+
+
