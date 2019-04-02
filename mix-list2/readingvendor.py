547bcf155cefec175f6addcdcc24b9c18246f703
thomyi <thomasyi@gmail.com>
2019-04-02 08:08:34 -0400

    yoour commit message

---
+++ mix-list2/readingvendor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env /python3
+"""using csv data"""
+import csv
+
+def main():
+    # open the csv data set
+    with open("vendor.csv", "r") as vendorfile:
+        vendata = csv.reader(vendorfile, delimiter=",")
+        # the csv data is now nicely prepared
+        # each new row is a python list
+        # we an reference the itmes we want to print 
+        # as we loop over the data
+        for row in vendata:
+            print("The IP address " + row[2] + \
+                    " requires the driver " + row[3])
+main()
