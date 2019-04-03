ef8e4a5645585149545fa87e908d0b2e33f49933
thomyi <thomasyi@gmail.com>
2019-04-03 08:06:40 -0400

    your commit message

--- dict01/ciscoAPICparser.py
+++ dict01/ciscoAPICparser.py
@@ -3,10 +3,10 @@
 import json # working with json data
 
 def main():
-	with open("ciscoAPIC.json", "r") as jsono file:
-	fog = jsono.load(jsonofile)
+    with open("ciscoAPIC.json", "r") as jsonfile:
+	fog = json.load(jsonfile)
 	print("The number of instances: ", len(fog['imdata']))
 	for instances in fog['imdata']:
-		print("Firmware version running - ", instances.get('firmwareCtrlrRunning').get('attributes'))
+	    print("Firmware version running - ", instances.get('firmwareCtrlrRunning').get('attributes'))
 
 main()
