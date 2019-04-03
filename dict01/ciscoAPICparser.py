d7edef20a190eb2b2a28dfb304336245eb45b2aa
thomyi <49160682+thomyi@users.noreply.github.com>
2019-04-03 08:27:18 -0400

    Add files via upload

--- dict01/ciscoAPICparser.py
+++ dict01/ciscoAPICparser.py
@@ -3,10 +3,10 @@
 import json # working with json data
 
 def main():
-    with open("ciscoAPIC.json", "r") as jsonfile:
-	fog = json.load(jsonfile)
-	print("The number of instances: ", len(fog['imdata']))
-	for instances in fog['imdata']:
-	    print("Firmware version running - ", instances.get('firmwareCtrlrRunning').get('attributes'))
+	with open("ciscoAPIC.json", "r") as json file:
+		fog = json.load(jsonfile)
+		print("The number of instances: ", len(fog['imdata']))
+		for instances in fog['imdata']:
+			print("Firmware version running - ", instances.get('firmwareCtrlrRunning').get('attributes'))
 
 main()
