b7a92a47368fc0c48d69696d61a22f3352a11913
thomyi <thomasyi@gmail.com>
2019-04-04 08:47:02 -0400

    Fin!

--- paramikosshra/sftpmover.py
+++ paramikosshra/sftpmover.py
@@ -7,4 +7,12 @@ import os
 
 ## where to connect to
 
-t
+t = paramiko.Transport("10.10.2.3", 22) ## IP and port
+
+## how to connect (see other labs on using id_rsa private / public keypairs)
+
+t = paramiko.Transport(username="bender", password="alta3")
+
+## Make an sftp connection object
+
+sftp = paramiko.SFTPClient.from : 
